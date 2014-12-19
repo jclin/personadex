f4edba121a9d9c4b39fb36b9b6d0318d9d3ab174
jclin <jclin18@gmail.com>
2014-12-18 20:16:33 -0500

    Plugged in images
    The thumbnails are wired up to be displayed in both the main view and details view. However, not all Persona names from the database match with a file name, so those will have to be fixed.

---
+++ Personadex/Utils/ImageSize.cs
@@ -0,0 +1,27 @@
+﻿using Windows.Foundation;
+
+namespace Personadex.Utils
+{
+    internal sealed class ImageSize
+    {
+        public static readonly ImageSize Small = new ImageSize(new Size(41, 41));
+        public static readonly ImageSize Large = new ImageSize(new Size(70, 70));
+
+        private readonly Size _dimensions;
+
+        private ImageSize(Size dimensions)
+        {
+            _dimensions = dimensions;
+        }
+
+        public override string ToString()
+        {
+            return string.Concat(_dimensions.Width, "x", _dimensions.Height);
+        }
+
+        public static implicit operator string(ImageSize imageSize)
+        {
+            return imageSize.ToString();
+        }
+    }
+}
